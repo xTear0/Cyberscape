e9bf429d6318a1e3843388cfb73bd1c208a85fee
Spencer Kotrosa <centaurusaurora@gmail.com>
2026-05-19 19:11:00 -0400

    Successful Build Files

--- Source/Cyberscape/Cyberscape.Build.cs
+++ Source/Cyberscape/Cyberscape.Build.cs
@@ -6,18 +6,28 @@ public class Cyberscape : ModuleRules
 {
 	public Cyberscape(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+     		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+     	
+     		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "PhysicsCore", "DedicatedServers" });
+     
+     		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTags", "Slate", "SlateCore" });
+     
+     		// Uncomment if you are using Slate UI
+     		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+     		
+     		// Uncomment if you are using online features
+     		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+     
+     		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+     		// Not needed at the moment as no files require this, it is moved to the DedicatedServers Module.
+     		/*if (Target.Type == TargetType.Server)
+     		{
+     			PublicDependencyModuleNames.Add("GameLiftServerSDK");
+     		}
+     		else
+     		{
+     			PublicDefinitions.Add("WITH_GAMELIFT=0");
+     		}*/
+     		bEnableExceptions =  true;
+     	}
 }
